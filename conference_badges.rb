b609671c205895aeeb8fe76db18901e78b9abb66
mborreros <mlbsindia@gmail.com>
2022-02-25 17:21:59 -0500

    commit tree 2e0c686fac1c16bccf2c3172c9caf39091d8f002

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  name_array = []
+  for name in array do
+     name_array << badge_maker(name)
+   end
+   name_array
+end
+
+def assign_rooms(array)
+  room_array = []
+  array.each_with_index { |name, index|
+  text = "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  room_array << text
+  }
+  room_array
+end
+
+def printer(array)
+  first_array = batch_badge_creator(array)
+  second_array = assign_rooms(array)
+  for element in first_array do
+    puts element
+  end
+  for element in second_array do
+    puts element
+  end
+end
